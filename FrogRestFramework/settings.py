ad7a41ebcc81cd5ed3390539e0c59e4878c81394
Liromanz <teo.tb@mail.ru>
2024-04-28 19:07:28 +0300

    попробовал поменять settings.py. это единственные изменения, которые были на сервере, но не было на локалке. вероятно в этом проблесма static. точно не в static_root, потому что до этого же работало

--- FrogRestFramework/settings.py
+++ FrogRestFramework/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7xrtef(jpnp_5f@*z^=fjt8l@drf9m#zsxlky&n00ku4#&7)g@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['83.222.11.137']
+ALLOWED_HOSTS = ['83.222.11.137', '0.0.0.0', '127.0.0.1']
 
 
 # Application definition
