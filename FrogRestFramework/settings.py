2eb7377918e5446386c65c1e4e237bed946496fd
Liromanz <teo.tb@mail.ru>
2024-04-28 19:21:34 +0300

    последний заезд

--- FrogRestFramework/settings.py
+++ FrogRestFramework/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_ROOT = '/root/frogapi/static'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
