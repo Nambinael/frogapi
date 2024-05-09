be47b05aa713956702c27bf10a9483637170b38f
Liromanz <teo.tb@mail.ru>
2024-05-09 16:14:58 +0300

    мне уже надоело эту статику туда сюда колыбасить

--- FrogRestFramework/settings.py
+++ FrogRestFramework/settings.py
@@ -128,10 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = 'FrogRestFramework/static/'
+STATIC_ROOT = 'static/'
 STATIC_URL = 'static/'
 
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'FrogRestFramework/static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
